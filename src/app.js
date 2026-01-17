a08ae49551ffe85b1979d8f528ee75deeaffd9c5
JBeanCode <js02bean@gmail.com>
2026-01-17 01:58:53 +0000

    josh-updated-array-names-to-plural-and-updated-button-naming-to-more-specific

--- src/app.js
+++ src/app.js
@@ -7,19 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-let who = ["My Grandpa","Batman","My chubby Aunt","George W. Bush", "The Rolling Stones", "Yoshi"]
-let action = ["ate","trampled","torched","crushed", "peed on", "ran off with"]
-let what = ["my phone","my TV","my car","my computer", "my house", "my girlfriend"]
-let when = ["when I was sleeping","when I was at McDonalds","when I was at the movies","when I was in class", "when I was 14"]
+let whos = ["My Grandpa","Batman","My chubby Aunt","George W. Bush", "The Rolling Stones", "Yoshi"]
+let actions = ["ate","trampled","torched","crushed", "peed on", "ran off with"]
+let whats = ["my phone","my TV","my car","my computer", "my house", "my girlfriend"]
+let whens = ["when I was sleeping","when I was at McDonalds","when I was at the movies","when I was in class", "when I was 14"]
 
 function getRandomIndex(array){
   return Math.floor(Math.random() * array.length)
 }
   
-const whoValue = who[getRandomIndex(who)]
-const actionValue = action[getRandomIndex(action)]
-const whatValue = what[getRandomIndex(what)]
-const whenValue = when[getRandomIndex(when)]
+const whoValue = whos[getRandomIndex(whos)]
+const actionValue = actions[getRandomIndex(actions)]
+const whatValue = whats[getRandomIndex(whats)]
+const whenValue = whens[getRandomIndex(whens)]
 
 const entireExcuse = `${whoValue} ${actionValue} ${whatValue} ${whenValue}`
 
@@ -29,10 +29,10 @@ let excuseLine = this.document.querySelector("#excuse")
   excuseLine.textContent = entireExcuse;
 };
 
-const myButton = document.getElementById("bigRedButton");
+const refreshButton = document.getElementById("bigRedButton");
 
 function handleClick() {
   location.reload(); // refreshes the page
 }
-myButton.addEventListener("click", handleClick);
+refreshButton.addEventListener("click", handleClick);
 
